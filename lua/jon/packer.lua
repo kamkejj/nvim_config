91e1737068433d14491e6c72e3a1c85eb4c05a10
Jon Kamke <jon.kamke@nerdery.com>
2024-02-21 20:04:45 -0600

    new plugins and keymaps

--- lua/jon/packer.lua
+++ lua/jon/packer.lua
@@ -18,5 +18,35 @@ return require('packer').startup(function(use)
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
   use('tpope/vim-fugitive')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
+		  -- {'williamboman/mason.nvim'},
+		  -- {'williamboman/mason-lspconfig.nvim'},
+
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+  }
+
+  use {
+	  "williamboman/mason.nvim",
+	  "williamboman/mason-lspconfig.nvim",
+	  "neovim/nvim-lspconfig",
+  }
+
+  use( 'mhartington/formatter.nvim')
+  use( 'theprimeagen/harpoon')
+  use('mbbill/undotree')
+
 end)
+
